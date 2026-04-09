eeef2c53bc54d6f15c815c5d7e36f9c2d0f893ee
Harvey Mendoza <93651968+harvzmendoza@users.noreply.github.com>
2026-04-09 20:57:11 +0800

    no user can login in admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers\Filament;
 
-use Filament\Http\Middleware\Authenticate;
+use App\Http\Middleware\EnsureUserIsNotUserRoleForAdminPanel;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
+use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -28,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin') 
+            ->path('admin')
             ->login()
             ->brandLogo(asset('images/pingo-full.png'))
             ->brandLogoHeight('3rem')
@@ -62,6 +63,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+                EnsureUserIsNotUserRoleForAdminPanel::class,
             ]);
     }
 }
